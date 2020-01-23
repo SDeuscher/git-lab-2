5d327e05e4a062132d933f98224ace97cb00e924
SDeuscher <sd621517@ohio.edu>
2020-01-22 19:42:41 -0500

    Hello Git

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+*       File: git-lab-program.cc
+*       Author: Spencer Deuscher
+*       Date: 1-22-20
+*       Description: Say hello git
+*/
+
+#include <iostream> 
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
